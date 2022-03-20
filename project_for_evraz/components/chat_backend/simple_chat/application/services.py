8c69d0e01f702314fc783f02fcc19f5670a78f2a
RewCrew <kerim94@bk.ru>
2022-03-20 12:27:02 +0300

    added all tests

--- project_for_evraz/components/chat_backend/simple_chat/application/services.py
+++ project_for_evraz/components/chat_backend/simple_chat/application/services.py
@@ -5,7 +5,7 @@ from pydantic import conint, validate_arguments
 from classic.app import DTO, validate_with_dto
 from classic.aspects import PointCut
 from classic.components import component
-from classic.messaging import Message, Publisher
+
 
 from . import interfaces, errors
 from .dataclasses import Chat, Message, User, ChatUsers
